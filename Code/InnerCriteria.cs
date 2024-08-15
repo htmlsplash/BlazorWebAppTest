3f71b394905447bd722e1737f1609908107c4f7a
Luke Kurach <lkurach@hotmail.com>
2024-08-15 14:09:46 -0400

    Criteria bug test commit

---
+++ Code/InnerCriteria.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWebAppTest.Code
+{
+	public class InnerCriteria
+	{
+		public string Filter2 { get; set; }
+
+		// Because it is a nullable datatype (using int data type resolves the problem or commenting this property out)
+		// I confirmed that any nullable type will cause the databind problem.
+		public int? ThisFieldDeclarationCausesSubmitBug { get; set; }
+	}
+}
---
+++ Code/OuterCriteria.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebAppTest.Code
+{
+	public class OuterCriteria
+	{
+		public int? SomeId { get; set; }
+		public string Filter1 { get; set; }
+        public InnerCriteria InnerCriteria { get; set; } = new InnerCriteria();
+    }
+}
